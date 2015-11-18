b7b260d44fbd1cdd443df6bd22519e222907c782
a-loner <sys_er26@msn.com>
2015-11-18 13:55:20 +0800

    establish this image converter

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a test ROS package for resize image stream from usb camera.
---
+++ src/img_cvt_node.cpp
@@ -0,0 +1,68 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+  
+public:
+  ImageConverter() : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, 
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/virtual_cam/image_raw", 1);
+
+  }
+
+  ~ImageConverter()
+  {
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Resize
+    cv_ptr->image= cv_ptr->image(cv::Rect(461,41,1000,1000));
+    //cv::Mat temp;
+    //cv_ptr->image.copyTo(temp);
+    //imwrite("~/Desktop/img/file.jpg",temp);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+
+  //Mat t = imread("lena.jpg");
+  //cv::namedWindow("OpenCV");
+  //imshow("OpenCV",t);
+  ros::spin();
+  //destroyWindow("OpenCV");
+
+  return 0;
+}
